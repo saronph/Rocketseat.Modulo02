5e51870314e4bf611805c89968671cb2ae4d869d
Saron Medeiros Philippi <54987514+saronph@users.noreply.github.com>
2019-11-11 10:40:07 -0300

    Fix auth

--- desafio02/src/routes.js
+++ desafio02/src/routes.js
@@ -8,11 +8,11 @@ import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.post('/students', StudentController.store);
-routes.put('/students/:id', StudentController.update);
-
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+
+routes.post('/students', StudentController.store);
+routes.put('/students/:id', StudentController.update);
 
 export default routes;
